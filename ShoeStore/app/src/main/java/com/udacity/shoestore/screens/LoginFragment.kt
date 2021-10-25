ac5ba63912fef0c3c0fce91c5445e7fb4ab68f5b
Michal Majernik <majernik.miso@gmail.com>
2021-10-24 22:20:18 -0700

    [TASK] Add viewModel for login data and validate them

--- ShoeStore/app/src/main/java/com/udacity/shoestore/screens/LoginFragment.kt
+++ ShoeStore/app/src/main/java/com/udacity/shoestore/screens/LoginFragment.kt
@@ -1,17 +1,23 @@
 package com.udacity.shoestore.screens
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
 
-    lateinit var binding: FragmentLoginBinding
+    private lateinit var binding: FragmentLoginBinding
+
+    private lateinit var viewModel: LoginViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,6 +30,26 @@ class LoginFragment : Fragment() {
             false
         )
 
+        viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
+        binding.loginViewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        binding.loginButton.setOnClickListener {
+            hideKeyboard(it)
+            if (!viewModel.isEmailValid()) {
+                Toast.makeText(context, getString(R.string.invalid_email), Toast.LENGTH_LONG).show()
+            } else if (!viewModel.isPasswordValid()) {
+                Toast.makeText(context, getString(R.string.invalid_password), Toast.LENGTH_LONG).show()
+            } else {
+                //TODO Navigate to Onboarding
+            }
+        }
+
         return binding.root
+    }
+
+    private fun hideKeyboard(view: View) {
+        val imm = requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
--- ShoeStore/app/src/main/java/com/udacity/shoestore/screens/LoginViewModel.kt
+++ ShoeStore/app/src/main/java/com/udacity/shoestore/screens/LoginViewModel.kt
@@ -1,7 +1,20 @@
 package com.udacity.shoestore.screens
 
+import android.util.Patterns
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class LoginViewModel : ViewModel() {
+
+    var email = MutableLiveData<String>()
+    var password = MutableLiveData<String>()
+
+    fun isEmailValid(): Boolean {
+        return !email.value.isNullOrEmpty() && Patterns.EMAIL_ADDRESS.matcher(email.value).matches()
+    }
+
+    fun isPasswordValid(): Boolean {
+        return !password.value.isNullOrEmpty()
+    }
 
 }
