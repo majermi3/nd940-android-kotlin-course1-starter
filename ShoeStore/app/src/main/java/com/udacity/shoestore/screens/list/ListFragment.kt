f3bc270f1e862de828c0aae9173742a54ca7c774
Michal Majernik <majernik.miso@gmail.com>
2021-10-25 20:26:23 -0700

    [TASK] List dummy shoes

--- ShoeStore/app/src/main/java/com/udacity/shoestore/screens/list/ListFragment.kt
+++ ShoeStore/app/src/main/java/com/udacity/shoestore/screens/list/ListFragment.kt
@@ -5,15 +5,68 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.FragmentListBinding
+import com.udacity.shoestore.models.Shoe
 
 class ListFragment : Fragment() {
+
+    private lateinit var binding: FragmentListBinding
+
+    private lateinit var viewModel: ListViewModel
+
+    var primaryColor: Int = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+    ): View {
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_list,
+            container,
+            false
+        )
+
+        viewModel = ViewModelProvider(this).get(ListViewModel::class.java)
+        binding.listViewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        primaryColor = ContextCompat.getColor(requireContext(), R.color.colorPrimary);
+
+        addShoesToList(binding.shoeList)
+
+        return binding.root
+    }
+
+    private fun addShoesToList(view: LinearLayout) {
+        viewModel.shoes.value?.forEach { shoe ->
+            addShoeToList(view, shoe)
+        }
+    }
+
+    private fun addShoeToList(view: LinearLayout, shoe: Shoe) {
+        val header = TextView(context)
+        header.text = "${shoe.company} - ${shoe.name}"
+        header.textSize = 24.0F
+        header.setTextColor(primaryColor)
+
+        val size = TextView(context)
+        size.text = getString(R.string.size_label, shoe.size.toString())
+        size.setPadding(16, 4, 0,0)
+
+
+        val description = TextView(context)
+        description.text = shoe.description
+        description.setPadding(16, 8,0, 32)
+
+        view.addView(header)
+        view.addView(size)
+        view.addView(description)
     }
 }
