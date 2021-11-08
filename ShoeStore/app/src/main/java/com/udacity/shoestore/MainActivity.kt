d190c3d2155f55ca025fabf7097300adf282a49a
Michal Majernik <majernik.miso@gmail.com>
2021-11-08 16:39:36 -0700

    [BUGFIX] Use DataBindingUtil to set the content view

--- ShoeStore/app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ ShoeStore/app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -2,17 +2,23 @@ package com.udacity.shoestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
+import com.udacity.shoestore.databinding.ActivityMainBinding
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
         Timber.plant(Timber.DebugTree())
 
         setSupportActionBar(findViewById(R.id.toolbar))
